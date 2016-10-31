cce0b90bcad834df484afc49ad86aab9cf2522d6
remozhang <814895077@qq.com>
2016-10-31 21:18:37 +0800

    10.31   2017新春贺卡
    完成了3个页面之间的交互 还差页面尺寸等问题

--- demo/慕课网新春贺卡/js/script.js
+++ demo/慕课网新春贺卡/js/script.js
@@ -1,8 +1,29 @@
 
 window.onload = function() {
+	var page1 = document.getElementById('page1');
+	var page2 = document.getElementById('page2');
+	var page3 = document.getElementById('page3');
 	var music = document.getElementById("music");
 	var audio = document.getElementsByTagName("audio")[0];
-	music.onclick = function() {
+	var lantern = document.getElementById("lantern")
+
+	// 音乐停止时，停止光盘旋转
+	audio.addEventListener("ended",function(){
+		music.setAttribute("class","")
+	},false);
+
+	// 点击音乐图标，控制音乐播放效果和光盘旋转
+	// music.onclick = function() {
+	// 	if (audio.paused) {
+	// 		audio.play();
+	// 		this.setAttribute("class","play");
+	// 	}else {
+	// 		audio.pause();
+	// 	    this.setAttribute("class","")
+	// 	}
+	// }
+
+	music.addEventListener("touchstart",function(event) {
 		if (audio.paused) {
 			audio.play();
 			this.setAttribute("class","play");
@@ -10,5 +31,22 @@ window.onload = function() {
 			audio.pause();
 		    this.setAttribute("class","")
 		}
-	}
+	},false);
+    
+    //点击屏幕一跳转效果
+    lantern.addEventListener("touchstart",function(event) {
+    	this.style.top ="-100%";
+    	// this.setAttribute("class","p1_lantern top")
+		setTimeout(function(){
+		    page1.style.display = "none";
+	    	page2.style.display = "block";
+	    	page3.style.display = "block";
+	    	page3.style.top = "100%";
+	        
+	        setTimeout(function() {
+				page2.setAttribute("class","page fadeout")
+				page3.setAttribute("class","page fadeIn")
+	        },5500);
+		},1000);
+    },false)    
 };
