8b9b1600c7995d874408c1bf7011b8f0e8451880
remozhang <814895077@qq.com>
2016-11-17 20:22:22 +0800

    上手webpack

---
+++ demo/webpack-test/webpack.config.js
@@ -0,0 +1,11 @@
+var Webpack = require("webpack");
+module.exports = {
+	entry: ["./entry.js"],
+	output: {
+		filename: "bundle.js" 
+	},
+	module: {
+		loaders: [
+		  {test:/\.css$/,loader:"style!css"}]
+	}
+}
