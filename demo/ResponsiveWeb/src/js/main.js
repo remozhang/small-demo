001858bf3d32937ac1c994b2b2fcbf3d8809377e
remozhang <814895077@qq.com>
2016-12-08 20:29:45 +0800

    响应式开发 大致是夭折回来了

--- demo/ResponsiveWeb/src/js/main.js
+++ demo/ResponsiveWeb/src/js/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
-	$("#owl-example").owlCarousel({
+	$(".owl-carousel").owlCarousel({
 		items: 1,
 		loop: true,
 		autoplay: true,
-		autoplayTimeout:3000,
+		autoplayTimeout:2000,
 		autoplayHoverPause: true,
-		dots: true,
 	});
 })
+
+
+// 根据屏幕大小选择合适的图片
+// $(function(){
+// 	function makeImageResponsive() {
+// 		var width = $(window).width();
+// 		var img = $(".ad img");
+// 		console.log(img)
+// 		if (width < 480) {
+// 			img.attr("src","img/ad001.png");
+// 		}else if (width < 800) {
+// 			img.attr("src","img/ad001-m.png")
+// 		}else {
+// 			img.attr("src","img/ad001-l.png")
+// 		}
+// 	}
+
+// 	$(window).on("resize load",makeImageResponsive)
+// })
